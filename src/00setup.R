807edb840d4017ea17a23687a6b0c485f9e3f7f4
Tom Purucker <purucker.tom@gmail.com>
2021-04-14 13:05:06 -0400

    /src and /data_in

---
+++ src/00setup.R
@@ -0,0 +1,27 @@
+#Install and load supporting libraries.
+print(Sys.info()[4])
+R.Version()$version.string
+
+library(sp)
+library(tiff)
+library(rgeos)
+library(rgdal)
+library(raster)
+library(dplyr)
+
+if(Sys.info()[4]=="LZ2626UTPURUCKE"){
+  root_dir <- file.path("c:", "git", "probabilistic_crop_McKaffrey")
+}else{
+  load('myEnvironment_prob_crop.RData')
+  root_dir <- 'C:/Users/epauluko/OneDrive - Environmental Protection Agency (EPA)/Profile/Documents/Paulukonis_Documents/manuscript_probabilistic_crop'
+}
+
+root_data_in <- file.path(root_dir, "data_in")
+root_data_out <- file.path(root_dir, "data_out")
+root_graphics <- file.path(root_dir, "graphics")
+root_src <- file.path(root_dir, "src")
+
+# data from ged
+crop_data_dir1 = file.path(root_data_in, "FinalCropsPt1")
+crop_data_dir2 = file.path(root_data_in,"FinalCropsPt2")
+merced_shp_dir = file.path(root_data_in,"cadwr_merced_shp")
