4ff14cfd6cde61ae957a0a6a1529225bded37e8a
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-04-16 10:15:35 -0400

    Update prob_crop_analysis_AP.R

--- prob_crop_analysis_AP.R
+++ prob_crop_analysis_AP.R
@@ -21,7 +21,6 @@ ddir2 = file.path(wd,"/FinalCropsPt2")
 setwd(ddir2)
 crop_stack2<-list.files(pattern='.tif$', all.files=TRUE, full.names=FALSE)
 
-
 #for some reason this is the only way it will let me stack for now (I think size)
 crop_stacka<- stack(crop_stack1)
 crop_stackb<- stack(crop_stack2)
@@ -31,13 +30,19 @@ crop_stackf<-stack(crop_stacka,crop_stackb)
 #read in clip of merced county fields
 merced <- readOGR(dsn = "C:/Users/epauluko/OneDrive - Environmental Protection Agency (EPA)/Profile/Documents/Paulukonis_Documents/manuscript_probabilistic_crop/prob_crop_data/cadwr_merced.shp")
 plot(merced) #quick peek at the field layer
-crs(merced)
-crs(crop_stackf)
+
+#read in county boundaries used by Kelly
+county <- readOGR(dsn = "C:/Users/epauluko/OneDrive - Environmental Protection Agency (EPA)/Profile/Documents/Paulukonis_Documents/manuscript_probabilistic_crop/prob_crop_data/CA_Counties_TIGER2016.shp")
+plot(county) #quick peek at the county layer
+plot(merced_c)
+merced_c <- spTransform(merced_c,crs(crop_stackf)) 
 mercedf <- spTransform(merced,crs(crop_stackf)) #transform crs of merced polygon
 ex<-extent(mercedf) #clip to extent of merced
 
 plot(crop_stacka[[5]]) 
-plot(mercedf, add=T)   # check to make sure that the merced plot is there
+plot(mercedf, add=T)
+plot(merced_c, add=T)# check to make sure that counties align with Kelly's polygons
+#they do; go ahead and use the CADWR field data 
 
 crop_stackf<-crop(crop_stackf,ex) #crop the crop stack to the merced polygon
 extent(crop_stackf) #check that extents match
@@ -45,9 +50,98 @@ extent(mercedf)
 crop_stackf<-stack(crop_stackf) #turn it back into a stack
 plot(crop_stackf[[2]])
 
+crs(mercedf) #check that crs is in m
+area_f<- as.data.frame(area(mercedf)) #area of each field in meters
+colnames(area_f)[1]<-'area_field'
+area_f$ID<-1:15925  #area_f
+
 ##Step 2: assign new probabilities (start here if loading previous environment)
 datff<- extract(crop_stackf, mercedf, df=T) #this extracts the raster cell values by polygons to a df
 sum_mat<-datff %>% group_by(ID) %>% summarise_all(funs(mean)) #summarize each crop by field to mean
+
+##whole dataset 
+#this is for the entire dataset, which takes too long to run, so we'll break it up eventually
+sum_c <- apply(sum_mat[,c(2:30)], 1, sum)
+sum_mat$NC<-round(1-sum_c,4) #add in column for non-crop
+sum_mat<-na.omit(sum_mat)
+mat_n<-as.data.frame(matrix(data=NA,nrow=nrow(sum_mat),ncol=1000))
+colnames(mat_n)[1:1000]<-paste0("Sim",1:1000,"")
+ID<-unique(sum_mat$ID)
+mat_n<-cbind(ID,mat_n)
+
+#let's get the total areas of each crop 
+#function to convert the rasters to binary
+fun_c <- function(x) {
+  x[x>0] <- 1
+  return(x)
+}
+out<-stack(calc(crop_stackf, fun_c))
+m<-list()
+#calculate the area (m2) of each type of raster (crop/non-crop) for each crop type
+for (i in 1:29) {
+  y <- out[[i]]
+  m[[i]]<-as.matrix(tapply(area(y), y[], sum))
+}
+
+m_a<-lapply(m, `length<-`, max(lengths(m))) #change length of list to all match
+area_c <- data.frame(matrix(unlist(m_a), nrow=length(m_a), byrow=TRUE)) #turn into workable dataframe for total crop area
+NC<-area_c[1,1]
+area_c<-as.data.frame(area_c[,2])
+area_c[30,]<-NC
+colnames(area_c)[1]<-'crop_total'
+#area_c$Crop<-colnames(mini_dat)[2:31] #add names to the list of crop area
+area_c[is.na(area_c)] <- 0 #add in 0 #this is the dataset containing the total crop areas for each crop
+area_c$field_tot<-0
+area_c<-as.data.frame(t(area_c))
+colnames(area_c)<-colnames(sum_mat)[2:31] 
+
+#dataset for updating the areas that matches ncol and nrow of mat_n
+area_up<-sum_mat[,2:31]
+area_up[,1:30]<-0
+area_up<-as.data.frame(area_up) 
+
+#see how long this takes
+sleep_for_a_minute <- function() { Sys.sleep(60) }
+start_time <- Sys.time()
+sleep_for_a_minute()
+end_time <- Sys.time()
+end_time - start_time
+
+for (j in 2:ncol(mat_n)){
+  for (i in 1:nrow(mat_n)){
+    out<-sum_mat[sum_mat$ID %in% mat_n[i,1],]
+    var<-out[,2:31] #pull out crops
+    var<-rbind(var, area_c)
+    #var_n<-ifelse(var[3,] == var[2,], 0, (1-(var[3,]/var[2,]))*var[1,]) #if total crop area = sum of field area, automatically assign 0 prob
+    var_n<-(1-(var[3,]/var[2,]))*var[1,]
+    var_n<-rapply(var_n, function(x) ifelse(is.nan(x),0,x), how="replace" ) 
+    var_n[var_n < 0] <- 0 #if any probs are neg, change to 0
+    probs<-as.numeric(var_n) 
+    r1<-sample(30, size = 1, replace = TRUE, prob = probs)
+    mat_n[i,j] <-colnames(out)[r1+1]
+    field_a<-area_f[area_f$ID %in% mat_n[i,1],]
+    area_up[i,]<-ifelse(mat_n[i,j] == names(area_up), field_a[,1], 0)
+    area_c[2,]<-colSums(area_up)
+  }
+}
+
+
+########everything below is scraps/experimental----
+#this is the original working loop
+for (j in 2:ncol(mat_n)){
+  for (i in 1:nrow(mat_n)){
+    out<-mini_dat[mini_dat$ID %in% mat_n[i,1],] 
+    var<-out[,2:31] #pull out crops
+    probs<-as.numeric(var)  
+    #reassign probability here 
+    r1<-sample(30, size = 1, replace = TRUE, prob = probs)
+    mat_n[i,j] <-colnames(out)[r1+1]
+  }
+  
+  #updating the area here
+}
+ 
+
 
 #let's create a mini dataset to work through
 mini_dat<-sum_mat[1:10,]
@@ -61,7 +155,6 @@ mat_n<-cbind(ID,mat_n)
 #let's get the total areas of each crop 
 #function to convert the rasters to binary
 fun_c <- function(x) {
-  #x[is.na(x[])] <- 0 #don't do this because it expands the area
   x[x>0] <- 1
   return(x)
 }
@@ -73,176 +166,47 @@ for (i in 1:29) {
   m[[i]]<-as.matrix(tapply(area(y), y[], sum))
 }
 #should output the total area (m2)for each crop as a list
-# prct<-lapply(m, function(x) x[2]/sum(x)) #percent of landscape that is this crop  
-# prct<-as.data.frame(unlist(prct)) #here is the percent of each crop
-# prct$crop<-colnames(sum_mat)[2:30] #add names to the prct
 m_a<-lapply(m, `length<-`, max(lengths(m))) #change length of list to all match
 area_c <- data.frame(matrix(unlist(m_a), nrow=length(m_a), byrow=TRUE)) #turn into workable dataframe for total crop area
 NC<-area_c[1,1]
 area_c<-as.data.frame(area_c[,2])
 area_c[30,]<-NC
-colnames(area_c)[1]<-'C'
-area_c$Crop<-colnames(mini_dat)[2:31] #add names to the list of crop area
+colnames(area_c)[1]<-'crop_total'
+#area_c$Crop<-colnames(mini_dat)[2:31] #add names to the list of crop area
 area_c[is.na(area_c)] <- 0 #add in 0 #this is the dataset containing the total crop areas for each crop
 area_c$field_tot<-0
+area_c<-as.data.frame(t(area_c))
+colnames(area_c)<-colnames(mini_dat)[2:31] 
 
 #dataset for updating the areas that matches ncol and nrow of mat_n
-area_up<-mini_dat
-area_up[,2:31]<-0
+area_up<-mini_dat[,2:31]
+area_up[,1:30]<-0
+area_up<-as.data.frame(area_up) #issue: tibbles don't work within the ifelse statement
 
 
-#now let's see if we can get it to work for our mat_n and mini_dat
-for (j in 2:ncol(mat_n)){ #by simulation
-    for (i in 1:nrow(mat_n)){ #by ID
-      out<-mini_dat[mini_dat$ID %in% mat_n[i,1],] #this pulls out the row of crop probabilities by ID
-      var<-out[,2:31] #only probs
-      probs<-as.numeric(var) #numeric for sample function
-      r1<-sample(30, size = 1, replace = TRUE, prob = probs) #use IDs of each crop, and then the probs associated with crop
-      mat_n[i,j] <-names(var[,r1]) #assign probs originally
-      mat_n[i,j]<-ifelse(mat_n[i,j] == area_c$Crop, (1-(area_c$field_tot/area_c$C)), NA)
-      #missing section where I reassign names instead of raw probabilities 
-    }
-    for (x in 2:ncol(area_up)){
-      area_o<-area_f[area_f$ID %in% out[,1], ] 
-      area_o<-area_o[,1]
-      area_up[i,x]<-ifelse(out[,1] == area_up$ID & mat_n[i,j] == names(area_up), area_o, 0)
-      area_c$field_tot<-colsums(area_up[,2:31])
-  }
-}
-
-
-
-
-
-#I'm doing something wrong here; I'm guessing it doesn't like the indices I'm using 
-
-########everything below is scraps/experimental
 #this is the original working loop
 for (j in 2:ncol(mat_n)){
   for (i in 1:nrow(mat_n)){
-    out<-min_dat[mini_dat$ID %in% mat_n[i,1],] 
+    out<-mini_dat[mini_dat$ID %in% mat_n[i,1],]
     var<-out[,2:31] #pull out crops
-    probs<-as.numeric(var)  
-    #reassign probability here 
+    var<-rbind(var, area_c)
+    var_n<-(1-(var[3,]/var[2,]))*var[1,]
+    var_n<-rapply(var_n, function(x) ifelse(is.nan(x),0,x), how="replace" )
+    probs<-as.numeric(var_n)  
     r1<-sample(30, size = 1, replace = TRUE, prob = probs)
-    mat_n[i,j] <-names(var[,r1])
+    mat_n[i,j] <-colnames(out)[r1+1]
+    field_a<-area_f[area_f$ID %in% mat_n[i,1],]
+    area_up[i,]<-ifelse(mat_n[i,j] == names(area_up), field_a[,1], 0)
+    area_c[2,]<-colSums(area_up)
   }
-  
-  #updating the area here
-}
- 
-
-crp_data<-as.data.frame(matrix(data=runif(30,0,1),nrow=10,ncol=3))
-colnames(crp_data)<-c('Almond','Corn','Soy')
-ID<-1:10
-crp_data<-cbind(ID,crp_data)
-
-mat_n<-as.data.frame(matrix(data=NA,nrow=nrow(mini_dat),ncol=1000))
-
-#function over rows
-#how do we add in an updated probability based on area?
-#1- (area if crop so far/total area of crop)
-
-
-f_c <- m_c[m_c$Crop %in% mat_n[i,j],]
-m_c[x,4] <-m_c[x,4] + f_c
-#try for a single row...how does this work?
-out<-mini_dat[mini_dat$ID %in% mat_n[1,1],]
-area_c<-area_f[area_f$ID %in% out[,1], ] 
-area_c<-area_c[,1]
-var<-out[,2:31] #pull out crops
-probs<-as.numeric(var)
-
-r1<-sample(30, size = 1, replace = TRUE, prob = probs) #use IDs of each crop, and then the probs associated with crop
-crp <-names(var[,r1]) #assign probs originally
-f_c<-m_c[m_c$Crop %in% crp,]
-
-m_c$area_field<-ifelse(f_c$Crop == m_c$Crop, area_c, NA)
-
-
-m_c[x,4] <-m_c[x,4] + f_c
-mat_n[i,j]<- 1 - (m_c[x,4]/m_c[x,2]) #update probs
-
-
-prob_fun<-function(y){
-  c<-m_c[m_c$Crop %in% names(y),] 
-  c<-c[,2:3]
-  n_prob<-1-(areac/c[,1])
-  return(n_prob)
 }
 
-ara<-area_f[area_f$ID %in% out[,1], ] #this is the area of the field ID
-mat_a<-ifelse(mat_n[i,x] == mat_a$crop, ara, NA) 
-
-# c <- m_c[m_c$Crop %in%  mat_n[i,1],]
-# c<-c[,2]
-# n_prob<- 1-(sum(mat_a[,x])/c) #1 value
-#probs<-as.numeric(var) 
-
-#where id of mat_n == id area_f, insert area_f$area_field
-#30 * 1000 - after we make assignment but before we move on to the next row,
-#we'll update the areas for that crop
-#so that the probs are updated by the ratio of area of crop x so far/total area of crop x
-#once we finish our 1000th assignment, we take that field area and add it to the appropriate cell 
-#after first field is done, then we add that field area to the appropriate crop area sim
-#update probs here/add in area spreadsheet - this reads from line 109
-#we'll have to convert the probs to 0 if total area crop x is full up by prob value
-#1-ratio - you can see if rounding takes care of potential neg numbers, but other options as well 
-#use IDs of each crop, and then the probs associated with crop
-#excellent, this works
-#next two tasks: 
-#a) add in the area constraints (using Kelly's probabilities)
-#b) put it in the for loop essentially the two step for loop will allow for us to update the area
-
-#we can go ahead and use kelly's analysis to fill in missing acreage
-#i.e., whatever percentage she has for the missing crops, use that to estimate those missing rows
-#use kelly's estimates entirely; just percent * total acerage of cropland by county
-#use all of kelly's areas, not CoA, s we can do in other states
-#then figure to do a comparison between coa and kelly's estimate 
-
-
-crs(mercedf) #check that crs is in m
-area_f<- as.data.frame(area(mercedf)) #area of each field in meters
-colnames(area_f)[1]<-'area_field'
-area_f$ID<-1:15925 
-#area_f is going to give us the area in m2 ##don't worry about this quite yet
-#so instead of using NASS to verify the acreage of the simulations
-#what we'll do instead is use the acres from the estimated probability
-
-
-#this is for the entire dataset, which takes too long to run, so we'll break it up eventually
-sum_c <- apply(sum_mat[,c(2:30)], 1, sum)
-sum_mat$NC<-round(1-sum_c,4) #add in column for non-crop
-sum_mat<-na.omit(sum_mat)
-mat_n<-as.data.frame(matrix(data=NA,nrow=nrow(sum_mat),ncol=1000))
-colnames(mat_n)[1:1000]<-paste0("Sim",1:1000,"")
-ID<-unique(sum_mat$ID)
-mat_n<-cbind(ID,mat_n)
-#field areas x sims empty matrix, this is updated by 110-114
-
-for (i in 1:nrow(mat_n)){
-  for (x in 2:ncol(mat_n)){
-    out<-sum_mat[sum_mat$ID %in% mat_n[i,1],] 
-    var<-out[,2:31] #pull out crops
-    probs<-as.numeric(var)
-    #update probs here/add in area spreadsheet - this reads from line 109
-    #we'll have to convert the probs to 0 if total area crop x is full up by prob value
-    #1-ratio - you can see if rounding takes care of potential neg numbers, but other options as well 
-    #use IDs of each crop, and then the probs associated with crop
-    r1<-sample(30, size = 1, replace = TRUE, prob = probs)
-    mat_n[i,x] <-names(var[,r1])
-  }
-  #30 * 1000 - after we make assignment but before we move on to the next row,
-  #we'll update the areas for that crop
-  #so that the probs are updated by the ratio of area of crop x so far/total area of crop x
-  #once we finish our 1000th assignment, we take that field area and add it to the appropriate cell 
-  #after first field is done, then we add that field area to the appropriate crop area sim
-} 
 
 
 
 
-#add in sys.time for end point 
+
+
 save.image(file='myEnvironment_prob_crop.RData')
 
 ##scraps/various sections
@@ -387,43 +351,7 @@ crp_r<-names(crp_r)  #here is the name for that field
 #write a function similar to the one below, use lapply
 #does Tom have suggestions or similar code? 
 #do we add in a threshold for probability?
-#1-prob of all crops
-
-#by_ID<-split(mat_n, list(mat$f), drop=T) 
-# x is the split empty mat_n
-# y is the split sum_mat
-# z is the area
-pct <- function(x,y,z) {
-  for (a in colnames(z)){
-    for (i in nrow(x)){
-      if ( sum(y[a]) < "some maximum")  sample(x[,2:30],1)
-      if ( length(x[x >= p]) == length(x) ) 
-        else return( length(x[x >= p]) / length(x) ) 
-    }}}
-
-#this returns index number of the corresponding crop
-#outside loop over fields
-#parallel process - something to speed up the processing
-#for area stuff, we'll send it to a different core
-#build it out without area so far, and 
-
-
-#I think what I'll have to do is do a crop-wise list 
-#what if we go ahead and sample, but then re-sample or randomly remove/change fields based on area?
-#ex: 
-#FOR EACH CROP: convert all fields that have a prob above 0 to 1
-#calculate total area of these fields by matching them to the IDs in a df containing the total area of each field
-#if the total sum area of all our Crop X is > the value for crop X reported in the NASS dataset
-#we randomly remove a few fields? change to what?
-
-#challenge: how do we incorporate some sort of spatial constraint by crop for the entire county? 
-#do we take the sum of each crop for the whole county? I am stuck on how to get the code to 'approach' a limit
-
-#if sum of crop [a] area in mat_n < sum crop [a] area in area, sample from column crop [a]
-#else if sum of crop [a] in mat_n >= sum crop [a], sample from other crop
-
-#other challenge; for a large portion of the fields, many of the crops average zero
-#do we skew towards values > 0 for those fields? 
+#1-prob of all crop
 
 
 
@@ -453,3 +381,43 @@ nass_c_f$Cover_Type
 missing<-c("Almonds", "Dry Beans", "Fallow/Idle Cropland","Honeydew Melons","Misc Vegs & Fruits","Pistachios","Sweet Potatoes")
 nass_c_f_m<-nass_c[nass_c$Cover_Type %in% missing,] 
 nass_c_f<-rbind(nass_c_f,nass_c_f_m)
+
+
+#now let's see if we can get it to work for our mat_n and mini_dat
+for (j in 2:ncol(mat_n)){ #by simulation
+  for (i in 1:nrow(mat_n)){ #by ID
+    out<-mini_dat[mini_dat$ID %in% mat_n[i,1],] #this pulls out the row of crop probabilities by ID
+    var<-out[,2:31] #only probs
+    probs<-as.numeric(var) #numeric for sample function
+    r1<-sample(30, size = 1, replace = TRUE, prob = probs) #use IDs of each crop, and then the probs associated with crop
+    mat_n[i,j] <-colnames(out)[r1+1] #assign probs originally
+    mat_n[i,j]<-ifelse(mat_n[i,j] == area_c$Crop, (1-(area_c$field_tot/area_c$C)), NA)
+  }
+  for (x in 2:ncol(area_up)){
+    area_o<-area_f[area_f$ID %in% out[,1], ] 
+    area_o<-area_o[,1]
+    area_up[i,x]<-ifelse(out[,1] == area_up$ID & mat_n[i,j] == names(area_up), area_o, 0)
+    area_c$field_tot<-colsums(area_up[,2:31])
+  }
+}
+
+for (i in 1:nrow(mat_n)){
+  for (x in 2:ncol(mat_n)){
+    out<-sum_mat[sum_mat$ID %in% mat_n[i,1],] 
+    var<-out[,2:31] #pull out crops
+    probs<-as.numeric(var)
+    #update probs here/add in area spreadsheet - this reads from line 109
+    #we'll have to convert the probs to 0 if total area crop x is full up by prob value
+    #1-ratio - you can see if rounding takes care of potential neg numbers, but other options as well 
+    #use IDs of each crop, and then the probs associated with crop
+    r1<-sample(30, size = 1, replace = TRUE, prob = probs)
+    mat_n[i,x] <-names(var[,r1])
+  }
+  #30 * 1000 - after we make assignment but before we move on to the next row,
+  #we'll update the areas for that crop
+  #so that the probs are updated by the ratio of area of crop x so far/total area of crop x
+  #once we finish our 1000th assignment, we take that field area and add it to the appropriate cell 
+  #after first field is done, then we add that field area to the appropriate crop area sim
+} 
+
+
