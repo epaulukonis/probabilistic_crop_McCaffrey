12bc129297bad26150601b6f5b4bca3edd5395a2
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-05-03 12:57:27 -0400

    Update 04verify_data_and_create_figures.R

--- src/04verify_data_and_create_figures.R
+++ src/04verify_data_and_create_figures.R
@@ -2,3 +2,10 @@
 simulate_start_time <- Sys.time()
 print("stepping into 04verify_data_and_create_figures.R")
 print(Sys.time())
+
+#create boxplot of each crop from the sims showing distribution of total crop areas and then the line with the original areas
+
+
+#figure showing reduction of autocorrelation?
+
+
