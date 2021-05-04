49baf771818883449589144da3f3b4b9ca2705be
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-05-04 15:04:31 -0400

    Update 04verify_data_and_create_figures.R

--- src/04verify_data_and_create_figures.R
+++ src/04verify_data_and_create_figures.R
@@ -11,11 +11,38 @@ print(Sys.time())
 mat_n_f<-merge(mat_n,area_f, by='ID')
 mat_n_f<-mat_n_f[,c(1,4,2:3)]
 
-cols<-names(mat_n_f[,3:4])
+test<-setNames(lapply(names(mat_n_f)[-2], function(x) cbind(mat_n_f[2], mat_n_f[x])), names(mat_n_f)[-2])
+test[1]<-NULL
+IDs<-mat_n_f$ID
+test<-lapply(test, cbind, IDs)
+colnames<-c("area_field",'sim','ID')
+test<-lapply(test, setNames, colnames)
 
-out<-mat_n_f %>% 
-  group_by(Sim1)%>% 
-  summarise(Area_Crop = sum(area_field))
+crop_area_calc<-function(x){
+x %>% 
+  group_by(sim)%>% 
+  summarise(Area_Crop = sum(area_field))}
+test_f<-lapply(test, crop_area_calc)
+
+
+
+mat_n_f<-merge(mat_n,area_f, by='ID')
+mat_n_f<-mat_n_f[,c(1,4,2:3)]
+
+test<-setNames(lapply(names(mat_n_f)[-2], function(x) cbind(mat_n_f[2], mat_n_f[x])), names(mat_n_f)[-2])
+test[1]<-NULL
+IDs<-mat_n_f$ID
+test<-lapply(test, cbind, IDs)
+colnames<-c("area_field",'sim','ID')
+test<-lapply(test, setNames, colnames)
+
+crop_area_calc<-function(x){
+  x %>% 
+    group_by(sim)%>% 
+    summarise(Area_Crop = sum(area_field))}
+test_f<-lapply(test, crop_area_calc)
+
+
 
 
 
