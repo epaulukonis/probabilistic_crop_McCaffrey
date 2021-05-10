fbaa3b2558c97020594f0c5c3b5ee1430a7f2d5f
Tom Purucker <purucker.tom@gmail.com>
2021-05-10 10:10:08 -0400

    update loaded modules for eap script

--- src/run_crops_mcc_eap.sh
+++ src/run_crops_mcc_eap.sh
@@ -11,12 +11,11 @@
 setenv TMPDIR /work/HONEYBEE/eap/run_crops_mcc
 
 module load intel/19.0.5
-module load R/3.6.2
-module load geos/3.8.0
-module load gdal-2.4.3/intel-19.0
-module load proj-5.2.0/intel-19.0
+module load R/4.0.3
+module load gcc/6.1.0
+module load geos/3.8.1
+module load gdal-3.1.3/intel-19.0
+module load proj-7.1.1/intel-19.0
 module load udunits-2.2.26/intel-19.0
-
-
 
 Rscript  00setup.R
