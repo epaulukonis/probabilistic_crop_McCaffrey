978e153ae5397077be49a4de50743de489ea7d2d
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-05-12 12:04:31 -0400

    Update 02create_fields_level_pdfs.R

--- src/02create_fields_level_pdfs.R
+++ src/02create_fields_level_pdfs.R
@@ -77,6 +77,7 @@ probs_by_fields$NC <- round((1-sum_c),4)#add in column for non-crop
 print(dim(probs_by_fields))
 print("omit fields which don't overlap with crop data")
 probs_by_fields <- na.omit(probs_by_fields) 
+print(dim(probs_by_fields))
 probs_by_fields$ID<-1:nrow(probs_by_fields)
 print(dim(probs_by_fields))
 probs_by_fields <- as.data.frame(c(probs_by_fields[,31], round(probs_by_fields [,1:30],4)))
