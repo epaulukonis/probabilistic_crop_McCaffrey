c9181c7c3d538fab4a9b2036ca40b953199b253b
Annie Paulukonis, M.S <elizabethpaulukonis@gmail.com>
2021-05-20 17:20:50 -0400

    madera

--- src/02create_fields_level_pdfs.R
+++ src/02create_fields_level_pdfs.R
@@ -35,10 +35,10 @@ if( file.exists(madera_filename)&&
 {
   # files exist so we will load the needed R objects
   print("files do exist, read them in")
-  county<-"sacramento"
+  county<-"madera"
   print('specify county here')
   print(county)
-  probs_by_fields<-readRDS(file = sacramento_filename)  #dataframe of field probs by county
+  probs_by_fields<-readRDS(file = madera_filename)  #dataframe of field probs by county
   print(dim(probs_by_fields))
   print("loading extracted field file for county of interest here")
   print(Sys.time())
@@ -94,8 +94,8 @@ if( file.exists(madera_filename)&&
   
   print("saving probs_by_fields object")
   print(Sys.time())
-  saveRDS(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_sac.rds"))
-  write.csv(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_sac.csv"))
+  saveRDS(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_mad.rds"))
+  write.csv(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_mad.csv"))
 
 }else{
   # files don't exist so we will create them
@@ -127,10 +127,10 @@ if( file.exists(madera_filename)&&
   saveRDS(sanjoaquin, file = file.path(root_data_out, "sanjoaquin.rds"))
   saveRDS(stanislaus, file = file.path(root_data_out, "stanislaus.rds"))
   
-  county<-"sacramento"
+  county<-"madera"
   print('specify county here')
   print(county)
-  probs_by_fields<-readRDS(file = sacramento_filename) #use extracted finished dataframe
+  probs_by_fields<-readRDS(file = madera_filename) #use extracted finished dataframe
   print("loading extracted field file for county of interest here")
   print(Sys.time())
   
@@ -182,8 +182,8 @@ if( file.exists(madera_filename)&&
   
   print("saving probs_by_fields object")
   print(Sys.time())
-  saveRDS(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_sac.rds"))
-  write.csv(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_sac.csv"))
+  saveRDS(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_mad.rds"))
+  write.csv(probs_by_fields, file = file.path(root_data_out, "probs_by_fields_mad.csv"))
   
 }
 
